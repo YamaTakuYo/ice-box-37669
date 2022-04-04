41058ab0ed71677897e8f5f38ed191272316fe1b
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-04-04 21:03:53 +0900

    Active Storage,アソシエーションの実装

---
+++ app/models/category.rb
@@ -0,0 +1,17 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '米類' },
+    { id: 3, name: '麺類' },
+    { id: 4, name: '肉類' },
+    { id: 5, name: '野菜' },
+    { id: 6, name: '海鮮' },
+    { id: 7, name: '卵・乳製品' },
+    { id: 8, name: '果物' },
+    { id: 9, name: '飲料' },
+    { id: 10, name: 'その他' }
+  ]
+    include ActiveHash::Associations
+    has_many :refrigerators
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         
+        has_many :refrigerator
+        
         with_options presence: true do
           validates :nickname
           validates :answer
         end
           
-        validates :secret_id, numericality: { other_than: 1 , message: "can't be blank"} 
+        validates :secret_id, numericality: { other_than: 1 , message: "can't be blank"}
 
         extend ActiveHash::Associations::ActiveRecordExtensions
           belongs_to :secret
