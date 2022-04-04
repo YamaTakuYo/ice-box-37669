692617a781f9f02b69049476637c8a160a281f74
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-04-04 14:11:47 +0900

    秘密の質問の編集

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,18 +1,14 @@
 class Secret < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '化粧水' },
-    { id: 3, name: '乳液' },
-    { id: 4, name: 'オールインワン' },
-    { id: 5, name: 'クレンジング' },
-    { id: 6, name: '洗顔料' },
-    { id: 7, name: '日焼け止め' },
-    { id: 8, name: 'リップ' },
-    { id: 9, name: '香水' },
-    { id: 10, name: 'ヘアカラー' },
-    { id: 11, name: 'ヘアスタイリング' },
-    { id: 12, name: 'シャンプー・コンディショナー' },
-    { id: 13, name: 'コンタクトレンズ' }
+    { id: 2, name: '出身中学校は' },
+    { id: 3, name: '好きな動物は' },
+    { id: 4, name: '子供時代を過ごした町の名前は' },
+    { id: 5, name: '飼っているペットの名前は' },
+    { id: 6, name: '初めて作った料理は' },
+    { id: 7, name: '憧れの職業は' },
+    { id: 8, name: '中学生時代の所属クラブは' },
+    { id: 9, name: '苦手な食べ物は' },
   ]
 
   include ActiveHash::Associations
