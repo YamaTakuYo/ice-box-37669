30d3b4cdd9ca41f6011a158f1758fb98dd12083d
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-04-04 11:17:33 +0900

    トップページの実装

---
+++ test/controllers/refrigerators_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class RefrigeratorsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get refrigerators_index_url
+    assert_response :success
+  end
+
+end
