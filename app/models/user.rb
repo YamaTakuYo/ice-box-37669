1fca8c4b475934497a2f4d8923fa21d0c9a1d55f
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-04-07 15:38:47 +0900

    ゲストユーザー機能　削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,16 +16,4 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :secret
   
-  def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.nickname = "ゲスト"
-      user.secret_id = "2"
-      user.answer = "AAA"
-      sign_in user
-      redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
-    end
-  end
 end
