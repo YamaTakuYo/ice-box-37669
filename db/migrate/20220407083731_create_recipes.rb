00fce1ec8e95e134b07944b3265ed90903a148a0
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-04-07 17:44:25 +0900

    テーブル作成

---
+++ db/migrate/20220407083731_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.references :user,        null: false, foreign_key: true
+      t.references :refrigerator,        null: false, foreign_key: true
+      t.string     :name,        null: false
+      t.integer    :ingredient, null: false
+      t.integer    :seasoning,        null: false
+      t.integer    :procedure, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_005119) do
+ActiveRecord::Schema.define(version: 2022_04_07_083731) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2022_04_05_005119) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "refrigerator_id", null: false
+    t.string "name", null: false
+    t.integer "ingredient", null: false
+    t.integer "seasoning", null: false
+    t.integer "procedure", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["refrigerator_id"], name: "index_recipes_on_refrigerator_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "refrigerators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -60,5 +73,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_005119) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "recipes", "refrigerators"
+  add_foreign_key "recipes", "users"
   add_foreign_key "refrigerators", "users"
 end
